d130f7f2a8ac0e51031607b710ae7d75e146e741
Ivan <soldren2@gmail.com>
2022-07-28 22:55:03 +0300

    Вывел булевый оператор  текстом с применением  функции if/else

--- lesson.py
+++ lesson.py
@@ -1,9 +1,46 @@
-Weather = input('Какая сегодня погода ? (дождь/снег/солнце) ')
-if Weather == 'дождь':
-    print('не забудь зонтик')
+# Упражнение 1
+# x=5
+# y=6
+# result=x<y
+# print(result)
 
-elif Weather == 'снег':
-    print('Не забудь варежки')
+from unicodedata import name
+from unittest import result
+
+# Упражнение 2
+
+
+
+
+
+x = str("(7<6) and (6<8)")
+
+y = str("(6>9) and (7>8)")
+
+if x == str("(5<6) and (6<8)"):
+
+ print(x)
+
 else:
-    print('Не забудь солнечные очки')
+      y= str("(6>9) and (7>8)")
 
+      print(y)
+
+#### Упражнение 3
+
+#Weather = input('Какая сегодня погода ? (дождь/снег/солнце) ')
+#if Weather == 'дождь':
+#print('не забудь зонтик')
+
+#elif Weather == 'снег':
+#print('Не забудь варежки')
+#else:
+#print('Не забудь солнечные очки')
+
+# Упражнение 4  на  возврат  номера и значения из списка
+
+#Rockets_player=['Маша', 'Даша', 'Миша' ]
+
+#print(Rockets_player[1])
+
+#print(Rockets_player.index('Маша'))
