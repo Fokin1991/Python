b736191581a8dbb859f6b376e4d784c4502ae176
Ivan <soldren2@gmail.com>
2022-09-25 00:02:39 +0300

    Текстовое упражение Python

--- lesson.py
+++ lesson.py
@@ -1,98 +1,13 @@
-# Упражвевие 1
-# x=5
-# y=6
-# result=x<y
-# print(result)
-
-
-# Упражвевие 2
-
-#x = str("(3<6) and (6<8)")
-#x=str(x)
-#y = str("(6>9) and (7>8)")
-
-#if str(x)==x:
-
-#print(x)
-
-#else:
-#y= str("(6>9) and (7>8)")
-
-#print(y)
-
-#### Упражвевие 3
-
-#Weather = input('Какая сегодвя погода ? (дождь/свег/солвце) ')
-#if Weather == 'дождь':
-#print('ве забудь зовтик')
-
-#elif Weather == 'свег':
-#print('ве забудь варежки')
-#else:
-#print('ве забудь солвечвые очки')
-
-# Упражвевие 4  ва  возврат  вомера и звачевия из списка
-
-#Rockets_player=['Маша', 'Даша', 'Миша' ]
-
-#print(Rockets_player[1])
-
-#print(Rockets_player.index('Маша'))
-
-##index=0
-##product=5
-##while index<product:
-#index=index + 1
-###print (product)
-
-
-
-#Упражнение  магазин
-
-#product_price=0
-#product = 0
-#sum=product_price*product
-#print('Продуктов в корзине ' +str(product))
-#answer=input('добавить  (д/н) ')
-
-#if answer=='д':
-    #product=product+1
-    #sum=product_price*product
-    #print('Фруктов в корзине ' +str(product))
-    #answer=input('добавить еще фруктов/убрать продукты/выйти из корзины (д/у/в)')
-#while answer=='д':
-    #product=product+1
-
-    #print('Фруктов в корзине ' +str(product))
-    #answer=input('добавить еще фруктов/убрать продукты/выйти из корзины (д/у/в)')
-#if answer=='у':
-     #product=product-1
-     #print('Продуктов в корзине ' +str(product))
-     #answer=input('добавить еще фруктов/убрать продукты/выйти из корзины (д/у/в)')
-     
-#else:
-    #print('Продуктов в корзине ' +str(product))
-
-### Циклы внутри цикла верхинй диапазон  задает  то количество раз скольколо 
-# в нем будет повторятся второе значение
-#  то есть  "победа" будет  повторена  3 раза  а  ура внутри в каждой из побед 5
-
-
-
-
-#for pobeda_counter in range(1,4):
-    #for ura_counter in range(1,6):
-        #print('ура')
-    #print('победа')
-
-
-
-
-
-x=int (input ('x: '))
-if x%2==0:
-    print(' четное')
-else:
-    print(' нечетное')
-
-print(x)
+1
+2
+3
+4
+import os
+ 
+ 
+os.system("cls")
+ 
+ 
+print("Как тебя зовут?")
+a=input(" ")
+print("Привет", a)
